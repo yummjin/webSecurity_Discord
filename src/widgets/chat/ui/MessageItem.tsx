c92957596c4f15daccbbaa17e47c7e333d349b32
yummjin <cho4u4o@gmail.com>
2025-11-28 01:14:15 +0900

    feat : add `dangerouslySetInnerHtml` to test xss attack

--- src/widgets/chat/ui/MessageItem.tsx
+++ src/widgets/chat/ui/MessageItem.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import { useEffect, useRef, type FC } from 'react';
 import type { Message } from '@/widgets/chat/types';
 import {
   messageItem,
@@ -18,10 +18,27 @@ interface MessageItemProps {
 }
 
 const MessageItem: FC<MessageItemProps> = ({ msg, sameGroup, index }) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      const scripts = contentRef.current.getElementsByTagName('script');
+      for (let i = 0; i < scripts.length; i++) {
+        const script = scripts[i];
+        const newScript = document.createElement('script');
+        newScript.text = script.text;
+        Array.from(script.attributes).forEach((attr) => {
+          newScript.setAttribute(attr.name, attr.value);
+        });
+        script.parentNode?.replaceChild(newScript, script);
+      }
+    }
+  }, [msg.content]);
+
   return sameGroup ? (
     <div key={index} className={messageItem}>
       <div className={MessageContent}>
-        <div className={textOnly}>{msg.content}</div>
+        <div ref={contentRef} className={textOnly} dangerouslySetInnerHTML={{ __html: msg.content }} />
       </div>
     </div>
   ) : (
@@ -32,7 +49,7 @@ const MessageItem: FC<MessageItemProps> = ({ msg, sameGroup, index }) => {
           <span className={username}>{msg.user.name}</span>
           <span className={timestamp}>{msg.timestamp}</span>
         </div>
-        <div className={text}>{msg.content}</div>
+        <div ref={contentRef} className={text} dangerouslySetInnerHTML={{ __html: msg.content }} />
       </div>
     </div>
   );
